06b9c8a9b78bf7427cbb24a835bfeab421a54ffb
Raul Acuna <racuna@rmr.tu-darmstadt.de>
2016-05-20 16:36:20 +0200

    Added the option to center the marker in a A4 format paper

--- pi-tag_gen.py
+++ pi-tag_gen.py
@@ -4,16 +4,16 @@ Author: Raul Acuna @ TU Darmstadt
 Email: raultron@gmail.com
 
 TODO :
-- Center the figure in a A4 paper
+- Convert to PDF
+- Print guiding lines to indicate the crossratios
 
 """
 import svgwrite
-from svgwrite import cm
+from svgwrite import cm, mm
 import argparse
 import sys
 
 def main(argv):
-    outputfile = 'test.svg'
     parser = argparse.ArgumentParser()
     parser.add_argument("marker_size", help="Length from center of top-left circle to center of top-right circle", default=12.5, type=float)
     parser.add_argument("circle_radius", help="Radius of each circle", default=0.9, type=float)
@@ -21,12 +21,24 @@ def main(argv):
     parser.add_argument("AC0", help="Cross relation AC in line 0",default=0.60, type=float)
     parser.add_argument("AB1", help="Cross relation AB in line 1",default=0.20, type=float)
     parser.add_argument("AC1", help="Cross relation AC in line 1",default=0.80, type=float)
-    
+    parser.add_argument("--A4", help="SVG output centered in A4 sheet size",action="store_true")
+    parser.add_argument("--pdf", help="Convert final SVG file to pdf",action="store_true")
+    parser.add_argument("--print_debug", help="Print debug information inside the marker",action="store_true")
+    #parser.add_argument("--print_debug", help="Print debug information inside the marker",action="store_true")
+
     args = parser.parse_args()
     
+    if args.A4:
+        w, h = 21.0,29.7
+        dwg = svgwrite.Drawing(filename='default.svg', size=(w*cm, h*cm), debug=True)    
+        dwg.add(dwg.rect(insert=(0,0), size=(w*cm, h*cm), fill='white', stroke='none'))
+    else:
+        w, h = '100%', '100%'
+        dwg = svgwrite.Drawing(filename='default.svg', size=(w, h), debug=True)    
+        dwg.add(dwg.rect(insert=(0,0), size=(w, h), fill='white', stroke='none'))
     
-    dwg = svgwrite.Drawing('default.svg')    
     
+
     #The four corners of the square
     marker_size = args.marker_size
     circle_radius = args.circle_radius
@@ -62,16 +74,22 @@ def main(argv):
     #center of the marker
     cx = cy = circle_clearance + circle_radius + marker_size/2
     
+    
+    
     #Squares for marker size reference
     size_inner_square = marker_size-circle_radius*2-circle_clearance*2
     corner_inner_square = 2*circle_radius + 2*circle_clearance
     size_outer_square = marker_size+circle_radius*2+circle_clearance*2
     
+    #We create a group for the whole marker
+    
+    marker = dwg.defs.add(dwg.g(id='marker'))
+    
     #Outer square
-    dwg.add(svgwrite.shapes.Rect((0*cm,0*cm),(size_outer_square*cm,size_outer_square*cm), fill='white', stroke='black'))
+    marker.add(svgwrite.shapes.Rect((0*cm,0*cm),(size_outer_square*cm,size_outer_square*cm), fill='white', stroke='black'))
     
     #inner square
-    dwg.add(svgwrite.shapes.Rect((corner_inner_square*cm,corner_inner_square*cm),(size_inner_square*cm,size_inner_square*cm), fill='none', stroke='black'))
+    marker.add(svgwrite.shapes.Rect((corner_inner_square*cm,corner_inner_square*cm),(size_inner_square*cm,size_inner_square*cm), fill='none', stroke='black'))
     
     
     
@@ -84,10 +102,10 @@ def main(argv):
     bottom_left_corner = (x1*cm,y2*cm)
     top_right_corner = (x2*cm,y1*cm)
     bottom_right_corner = (x2*cm,y2*cm)
-    dwg.add(svgwrite.shapes.Circle(top_left_corner, circle_radius*cm))
-    dwg.add(svgwrite.shapes.Circle(bottom_left_corner, circle_radius*cm))
-    dwg.add(svgwrite.shapes.Circle(top_right_corner, circle_radius*cm))
-    dwg.add(svgwrite.shapes.Circle(bottom_right_corner, circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle(top_left_corner, circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle(bottom_left_corner, circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle(top_right_corner, circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle(bottom_right_corner, circle_radius*cm))
     
     
     
@@ -96,32 +114,37 @@ def main(argv):
     
     
     
-    dwg.add(svgwrite.shapes.Circle(((x1+marker_size*CR_Line0_AB)*cm,y1*cm), circle_radius*cm))
-    dwg.add(svgwrite.shapes.Circle((x1*cm,(y1+marker_size*CR_Line0_AB)*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle(((x1+marker_size*CR_Line0_AB)*cm,y1*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle((x1*cm,(y1+marker_size*CR_Line0_AB)*cm), circle_radius*cm))
     
-    dwg.add(svgwrite.shapes.Circle(((x1+marker_size*CR_Line0_AC)*cm,y1*cm), circle_radius*cm))
-    dwg.add(svgwrite.shapes.Circle((x1*cm,(y1+marker_size*CR_Line0_AC)*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle(((x1+marker_size*CR_Line0_AC)*cm,y1*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle((x1*cm,(y1+marker_size*CR_Line0_AC)*cm), circle_radius*cm))
     
     #Now we draw the Cross related circles for line 1
-    dwg.add(svgwrite.shapes.Circle(((x1+marker_size*CR_Line1_AB)*cm,y2*cm), circle_radius*cm))
-    dwg.add(svgwrite.shapes.Circle((x2*cm,(y1+marker_size*CR_Line1_AB)*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle(((x1+marker_size*CR_Line1_AB)*cm,y2*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle((x2*cm,(y1+marker_size*CR_Line1_AB)*cm), circle_radius*cm))
     
-    dwg.add(svgwrite.shapes.Circle(((x1+marker_size*CR_Line1_AC)*cm,y2*cm), circle_radius*cm))
-    dwg.add(svgwrite.shapes.Circle((x2*cm,(y1+marker_size*CR_Line1_AC)*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle(((x1+marker_size*CR_Line1_AC)*cm,y2*cm), circle_radius*cm))
+    marker.add(svgwrite.shapes.Circle((x2*cm,(y1+marker_size*CR_Line1_AC)*cm), circle_radius*cm))
     
     
     #Put text with information about the marker in the center
-    dwg.add(dwg.text('Marker Size {0}'.format(marker_size), insert=((cx-4)*cm, (cy+3)*cm), fill='black'))
-    dwg.add(dwg.text('Crossratio 0: AB = {0:2.2f},  AC= {1:.2f}, CR= {2:.4f}'.format(CR_Line0_AB,CR_Line0_AC, cross_ration_0,4), insert=((cx-4)*cm, (cy+3+0.4)*cm), fill='black'))
-    dwg.add(dwg.text('Crossratio 1: AB = {0:2.2f},  AC= {1:.2f}, CR= {2:.4f}'.format(CR_Line1_AB,CR_Line1_AC, cross_ration_1,4), insert=((cx-4)*cm, (cy+3+0.8)*cm), fill='black'))
+    t_x = cx - 5
+    t_y = cy + size_outer_square/2 +1
+    
+    marker.add(dwg.text('| AB | BD | CD |', insert=((t_x+2.2)*cm, t_y*cm), fill='black', textLength = 10*cm))
+    marker.add(dwg.text('| {0:.3f}cm | {1:.3f}cm | {2:.3f}cm |'.format(d_line0_AB,d_line0_BD,d_line0_CD), insert=((t_x+2.2)*cm, (t_y+0.5)*cm), fill='black'))
+    marker.add(dwg.text('| {0:.3f}cm | {1:.3f}cm | {2:.3f}cm |'.format(d_line1_AB,d_line1_BD,d_line1_CD), insert=((t_x+2.2)*cm, (t_y+1.0)*cm), fill='black'))    
+    marker.add(dwg.text('Line Top:', insert=((t_x)*cm, (t_y+0.5)*cm), fill='black'))
+    marker.add(dwg.text('Line Bottom:', insert=((t_x)*cm, (t_y+1.0)*cm), fill='black'))    
+    
+    marker.add(dwg.text('Marker Size {0}'.format(marker_size), insert=((t_x)*cm, (t_y+2)*cm), fill='black'))
+    marker.add(dwg.text('Crossratio 0: AB = {0:2.2f},  AC= {1:.2f}, CR= {2:.4f}'.format(CR_Line0_AB,CR_Line0_AC, cross_ration_0,4), insert=((t_x)*cm, (t_y+2+0.5)*cm), fill='black'))
+    marker.add(dwg.text('Crossratio 1: AB = {0:2.2f},  AC= {1:.2f}, CR= {2:.4f}'.format(CR_Line1_AB,CR_Line1_AC, cross_ration_1,4), insert=((t_x)*cm, (t_y+2+1)*cm), fill='black'))
     
     
     
-    dwg.add(dwg.text('| AB | BD | CD |', insert=((cx-3)*cm, (cy-5)*cm), fill='black', textLength = 10*cm))
-    dwg.add(dwg.text('| {0:.3f}cm | {1:.3f}cm | {2:.3f}cm |'.format(d_line0_AB,d_line0_BD,d_line0_CD), insert=((cx-3)*cm, (cy-4.5)*cm), fill='black'))
-    dwg.add(dwg.text('| {0:.3f}cm | {1:.3f}cm | {2:.3f}cm |'.format(d_line1_AB,d_line1_BD,d_line1_CD), insert=((cx-3)*cm, (cy-4)*cm), fill='black'))    
-    dwg.add(dwg.text('Line Top:', insert=((cx-5.2)*cm, (cy-4.5)*cm), fill='black'))
-    dwg.add(dwg.text('Line Bottom:', insert=((cx-5.2)*cm, (cy-4)*cm), fill='black'))
+    
     
     
     
@@ -138,12 +161,25 @@ def main(argv):
     d_line1_CD = marker_size - marker_size * args.AC1 #;//CD
     
     #red do in the Center of the marker
-    dwg.add(svgwrite.shapes.Circle((cx*cm,cy*cm), 0.05*cm, fill='red'))
+    marker.add(svgwrite.shapes.Circle((cx*cm,cy*cm), 0.05*cm, fill='red'))
     
-    dwg.add(dwg.line((cx*cm, cy*cm), ((cx+2)*cm, cy*cm), stroke=svgwrite.rgb(10, 10, 16, '%')))
-    dwg.add(dwg.line((cx*cm, cy*cm), (cx*cm, (cy-2)*cm), stroke=svgwrite.rgb(10, 10, 16, '%')))
-    dwg.add(dwg.text('x', insert=((cx+2.1)*cm, (cy)*cm), fill='black'))
-    dwg.add(dwg.text('y', insert=((cx)*cm, (cy-2.2)*cm), fill='black'))
+    marker.add(dwg.line((cx*cm, cy*cm), ((cx+2)*cm, cy*cm), stroke=svgwrite.rgb(10, 10, 16, '%')))
+    marker.add(dwg.line((cx*cm, cy*cm), (cx*cm, (cy-2)*cm), stroke=svgwrite.rgb(10, 10, 16, '%')))
+    marker.add(dwg.text('x', insert=((cx+2.1)*cm, (cy)*cm), fill='black'))
+    marker.add(dwg.text('y', insert=((cx)*cm, (cy-2.2)*cm), fill='black'))
+    
+        
+    if args.A4:
+        #center marker in middle of A4 sheet
+        x_m = ((w/2)-(size_outer_square/2))
+        y_m = ((h/2)-(size_outer_square/2))
+        u = dwg.use(marker, insert=(x_m*cm,y_m*cm))
+    else:
+        u = dwg.use(marker, insert=(0*cm,0*cm))
+        
+    
+    
+    dwg.add(u)
     
     dwg.save()
 
